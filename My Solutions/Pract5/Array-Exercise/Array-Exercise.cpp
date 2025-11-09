eaf52a48f1474f13b394eac747aa1fc26fd025b8
TeodorIliev06 <tedo2505@gmail.com>
2025-11-09 11:09:24 +0200

    chore: add advanced array manipulation examples
    - implement custom union and intersection
    - implement longest decreasing subsequence

--- My Solutions/Pract5/Array-Exercise/Array-Exercise.cpp
+++ My Solutions/Pract5/Array-Exercise/Array-Exercise.cpp
@@ -16,12 +16,19 @@ void printArr(int* arr, size_t size) {
 	}
 }
 
+void copyArr(const int* source, size_t size, int* destination) {
+	for (size_t i = 0; i < size; i++)
+	{
+		destination[i] = source[i];
+	}
+}
+
 double getAbsValue(double num) {
 	return (num > 0) ? num : -num;
 }
 
 /// <summary>
-/// Compute arithmetic mean (AM) and the closest element to it
+/// Compute arithmetic mean (AM) and find the closest element to it
 /// </summary>
 /// <param name="arr"></param>
 /// <param name="size"></param>
@@ -114,6 +121,235 @@ bool isSymetric(const int* arr, size_t size) {
 	return true;
 }
 
+unsigned getLongestDecreasingSubsequence(const int* arr, size_t size) {
+	if (size == 0)
+	{
+		return 0;
+	}
+
+	unsigned currentSeqLength = 1, bestSeqLength = 1;
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+		{
+			currentSeqLength++;
+
+			if (bestSeqLength < currentSeqLength)
+			{
+				bestSeqLength = currentSeqLength;
+			}
+		}
+		else
+		{
+			currentSeqLength = 1;
+		}
+	}
+
+	return bestSeqLength;
+}
+
+unsigned getLongestSubsequenceOfEquals(const int* arr, size_t size) {
+	if (size == 0)
+	{
+		return 0;
+	}
+
+	unsigned currentSeqLength = 1, bestSeqLength = 1;
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		if (arr[i] == arr[i + 1])
+		{
+			currentSeqLength++;
+
+			if (bestSeqLength < currentSeqLength)
+			{
+				bestSeqLength = currentSeqLength;
+			}
+		}
+		else
+		{
+			currentSeqLength = 1;
+		}
+	}
+
+	return bestSeqLength;
+}
+
+void setAsNull(int* arr, size_t size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		int currentNum = arr[i];
+		bool isContained = false;
+
+		while (currentNum > 0)
+		{
+			int currentDigit = currentNum % 10;
+
+			if (currentDigit == i)
+			{
+				isContained = true;
+				break;
+			}
+
+			currentNum /= 10;
+		}
+
+		if (!isContained)
+		{
+			arr[i] = 0;
+		}
+	}
+}
+
+void swap(int& firstNum, int& secondNum) {
+	int temp = firstNum;
+	firstNum = secondNum;
+	secondNum = temp;
+}
+
+void bubbleSort(int* arr, size_t size) {
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		bool isSwapped = false;
+
+		for (size_t j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+				isSwapped = true;
+			}
+		}
+
+		if (!isSwapped)
+		{
+			break;
+		}
+	}
+}
+
+int getFrequencyCount(const int* arr, size_t size, int key) {
+	int count = 0;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] == key)
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
+int getMostFrequentNumber(const int* arr, size_t size, int* copy) {
+	if (size == 1)
+	{
+		return arr[0];
+	}
+
+	// Find the most frequent in the copy
+	int maxFrequency = 1, currentFrequnecy = 1;
+
+	for (size_t i = 1; i < size; i++)
+	{
+		if (copy[i] == copy[i - 1])
+		{
+			currentFrequnecy++;
+
+			if (currentFrequnecy > maxFrequency)
+			{
+				maxFrequency = currentFrequnecy;
+			}
+		}
+		else
+		{
+			currentFrequnecy = 1;
+		}
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		int count = getFrequencyCount(copy, size, arr[i]);
+
+		if (count == maxFrequency)
+		{
+			return arr[i];
+		}
+	}
+
+	return arr[0];
+}
+
+void customIntersect(const int* firstArr, size_t firstArrSize,
+	const int* secondArr, size_t secondArrSize, int* result, size_t& intersectionSize) {
+	size_t resultPos = 0;
+
+	for (size_t i = 0; i < firstArrSize; i++)
+	{
+		bool isAddedToResult = false;
+		for (size_t k = 0; k < resultPos; k++)
+		{
+			if (result[k] == firstArr[i])
+			{
+				isAddedToResult = true;
+				break;
+			}
+		}
+
+		if (isAddedToResult)
+		{
+			continue;
+		}
+
+		for (size_t j = 0; j < secondArrSize; j++)
+		{
+			if (firstArr[i] == secondArr[j])
+			{
+				result[resultPos++] = firstArr[i];
+				break;
+			}
+		}
+	}
+
+	intersectionSize = resultPos;
+}
+
+void customUnion(const int* firstArr, size_t firstArrSize,
+	const int* secondArr, size_t secondArrSize, int* result, size_t& unionSize) {
+	size_t resultPos = 0;
+
+	// fill the union with one array
+	// then compare with the other
+
+	copyArr(firstArr, firstArrSize, result);
+	resultPos = firstArrSize;
+
+	for (size_t i = 0; i < secondArrSize; i++)
+	{
+		bool isAddedToResult = false;
+		for (size_t k = 0; k < resultPos; k++)
+		{
+			if (result[k] == secondArr[i])
+			{
+				isAddedToResult = true;
+				break;
+			}
+		}
+
+		if (isAddedToResult)
+		{
+			continue;
+		}
+
+		result[resultPos++] = secondArr[i];
+	}
+
+	unionSize = resultPos;
+}
+
 int main()
 {
 	// 1
@@ -169,13 +405,89 @@ int main()
 	printArr(result, resultSize);*/
 
 	// 5
-	int arr[ARRAY_SIZE];
+	//int arr[ARRAY_SIZE];
 
-	size_t size;
-	std::cin >> size;
+	//size_t size;
+	//std::cin >> size;
 
-	readArr(arr, size);
+	//readArr(arr, size);
 
-	std::cout << std::boolalpha <<
-		(isSymetric(arr, size) ? "yes" : "no") << std::endl;
+	//std::cout << std::boolalpha <<
+	//	(isSymetric(arr, size) ? "yes" : "no") << std::endl;
+
+	// 6
+	//int arr[ARRAY_SIZE];
+
+	//size_t size;
+	//std::cin >> size;
+
+	//readArr(arr, size);
+
+	//unsigned seqLength = getLongestDecreasingSubsequence(arr, size);
+	//std::cout << seqLength << std::endl;
+
+	// 7
+	//int arr[ARRAY_SIZE];
+
+	//size_t size;
+	//std::cin >> size;
+
+	//readArr(arr, size);
+
+	//unsigned seqLength = getLongestSubsequenceOfEquals(arr, size);
+	//std::cout << seqLength << std::endl;
+
+	// 8
+	//int arr[ARRAY_SIZE];
+
+	//size_t size;
+	//std::cin >> size;
+
+	//readArr(arr, size);
+	//setAsNull(arr, size);
+	//printArr(arr, size);
+
+	// 9
+	//int arr[ARRAY_SIZE];
+	//int copy[ARRAY_SIZE];
+
+	//size_t size;
+	//std::cin >> size;
+
+	//readArr(arr, size);
+
+	//copyArr(arr, size, copy);
+	//bubbleSort(copy, size);
+
+	//std::cout << getMostFrequentNumber(arr, size, copy) << std::endl;
+
+	// 10
+	/*int firstArr[ARRAY_SIZE], secondArr[ARRAY_SIZE];
+
+	size_t firstArrSize, secondArrSize;
+
+	std::cout << "Please enter first sequence size" << std::endl;
+	std::cin >> firstArrSize;
+	std::cout << "Please enter first sequence of numbers" << std::endl;
+	readArr(firstArr, firstArrSize);
+
+	std::cout << "Please enter second sequence size" << std::endl;
+	std::cin >> secondArrSize;
+	std::cout << "Please enter second sequence of numbers" << std::endl;
+	readArr(secondArr, secondArrSize);
+
+	int intersectionArr[ARRAY_SIZE];
+	size_t intersecitonSize = 0;
+	customIntersect(firstArr, firstArrSize, secondArr, secondArrSize, intersectionArr, intersecitonSize);
+
+	std::cout << "Intersection: ";
+	printArr(intersectionArr, intersecitonSize);
+	std::cout << std::endl;
+
+	int unionArr[2 * ARRAY_SIZE];
+	size_t unionSize = 0;
+	customUnion(firstArr, firstArrSize, secondArr, secondArrSize, unionArr, unionSize);
+
+	std::cout << "Union: ";
+	printArr(unionArr, unionSize);*/
 }
